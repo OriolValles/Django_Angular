b11a1725c476725b2c5839ce767aae07677d95a4
OriolValles <oriolvc89@gmail.com>
2021-04-22 12:38:53 +0200

    Add testing

--- backend/apirest/tests.py
+++ backend/apirest/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from apirest.models import Printer
+from rest_framework.test import force_authenticate
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+
+class PrinterTests(APITestCase):
+    def test_create_Printer_without_user(self):
+        """
+        Ensure we can create a new Printer object.
+        """
+        url = reverse('printer-list')
+        data = {'name': 'Printer1'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_Printer_with_user(self):
+        """
+        Ensure we can create a new Printer object.
+        """
+        client = APIClient()
+        client.login(username='admin', password='password123')
+
+        url = reverse('printer-list')
+        data = {'name': 'Printer1'}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Printer.objects.count(), 1)
+        self.assertEqual(Printer.objects.get().name, 'DabApps')
+        # Log out
+        client.logout()
