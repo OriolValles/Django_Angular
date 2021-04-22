d2942c6489a286c46b97eb649adbdbe16be9df46
OriolValles <oriolvc89@gmail.com>
2021-04-22 11:38:19 +0200

    Add some views for test and help with apirest

--- backend/apirest/views.py
+++ backend/apirest/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from backend.apirest.serializers import PrinterSerializer
+
+
+class PrinterViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows printers to be viewed or edited.
+    """
+    queryset = Printer.objects.all().order_by('-date_joined')
+    serializer_class = PrinterSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
