324141b5f45994dc8cba70418b847a26b59f0e63
OriolValles <oriolvc89@gmail.com>
2021-04-22 11:22:37 +0200

    Creating model Printer

--- backend/apirest/models.py
+++ backend/apirest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Printer(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['created']
